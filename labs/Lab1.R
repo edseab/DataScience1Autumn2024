ac4cdd576ae4dedcd37cfe1abcc18ee52242a272
Yeboah10 <acquahyeboah01@gmail.com>
2024-10-22 12:56:02 +0100

    reverted

--- labs/Lab1.R
+++ labs/Lab1.R
@@ -157,6 +157,8 @@ my.vector == 'is'
 ### 2.1
 digits <- 0:10
 # Using the least amount of code possible, write a line of code that returns only the odd values of the digits object.
+digits[digits%%2==1]
+
 
 # Another important logical operator is the %in% operator. It tells you if the elements on the left are found in the elements on the right. E.G.
 group1 <- c('Arthur', 'Fatima', 'Suleiman', 'Marco')
