85b664fbcbdca4d77c0d5dcffb16e2496be109c4
Yeboah10 <acquahyeboah01@gmail.com>
2024-10-28 23:49:21 +0100

    Exercise completed

--- labs/Lab2.R
+++ labs/Lab2.R
@@ -1,4 +1,5 @@
 team###################################
+team###################################
 ###################################
 ########                   ########
 ########   Data Science 2  ########
@@ -49,18 +50,15 @@ colnames(star_wars_matrix) <- c("US revenue", "International revenue")
 # Use one of the above functions to calculate the total revenue for each movie (the sum of the US and international revenue)
 # and save it in an object called total_revenue
 total_revenue <- rowSums(star_wars_matrix)
+total_revenue <- rowSums(star_wars_matrix)
 # We can now add this vector as a new column using the function cbind (column bind)
 
+total_revenue
 total_revenue
 
 ### 1.2
 # Rename the 3rd element of the column names of star_wars_matrix to "Total revenue"
 colnames(star_wars_matrix)[3]<- "Total revenue"
-star_wars_matrix
-<<<<<<< HEAD
-star_wars_matrix
-=======
->>>>>>> 4d768c0 (Exercise completed)
 # Now lets create vectors for the box office returns of the prequel trilogy
 
 phantom_menace <- c(474.5,552.5)
@@ -68,9 +66,17 @@ attack_of_clones <- c(310.7,338.7)
 revenge_of_sith <- c(380.3, 468.5)
 
 
+
 ### 1.3
 # Turn these 3 vectors into a matrix, add a column for total revenue, 
 # and append them to star_wars_matrix using the function rbind (row bind)
+trilogy <- c(phantom_menace, attack_of_clones, revenge_of_sith)
+trilogy_matrix <- matrix(trilogy, byrow = T, nrow = 3)
+rownames(trilogy_matrix) <- c("The Phanton Menace", "Attack of Clones", "Revenge of Sith")
+colnames(trilogy_matrix) <- c("US revenue", "International revenue")
+total_revenue_tr <- rowSums(trilogy_matrix)
+trilogy_matrix <- cbind(trilogy_matrix,total_revenue_tr)
+star_wars_matrix <- rbind(star_wars_matrix, trilogy_matrix)
 trilogy <- c(phantom_menace, attack_of_clones, revenge_of_sith)
 trilogy_matrix <- matrix(trilogy, byrow = T, nrow = 3)
 rownames(trilogy_matrix) <- c("The Phanton Menace", "Attack of Clones", "Revenge of Sith")
@@ -92,6 +98,7 @@ star_wars_matrix[3,1]
 
 ### 1.4 
 # Write a line of code to extract the international revenue of the Phantom Menace (the 4th movie) using numbers to index the matrix
+star_wars_matrix[4,2]
 star_wars_matrix[4,2]
 # Compare this to the following:
 star_wars_matrix["The Phantom Menace", "Total revenue"]
@@ -139,6 +146,7 @@ my_list$boolean
 
 ### 2.1
 # Using the $ operator, replace the "matrix" element of my_list with the star_wars_matrix
+mylist$matrix <- star_wars_matrix
 mylist$matrix <- star_wars_matrix
 # Finally, you can turn any list into a vector with unlist().
 unlist(my_list)
@@ -227,6 +235,10 @@ binomial_function <- function(n,k,p){
   combination = choose(n,k)
   return((p^k)*((1-p)^(n-k))*combination)
 }
+binomial_function <- function(n,k,p){
+  combination = choose(n,k)
+  return((p^k)*((1-p)^(n-k))*combination)
+}
 
 # use your function to calculate the probability that when the aliens send 10 probes to Earth (probability of water = 0.7),
 # exactly 8 of those probes will send a signal of water
@@ -279,6 +291,8 @@ qnorm(0.1,175, 10, lower.tail=FALSE)
 # Let's run a Welch's t-test comparing the heights of men and women in class
 men_heights <- c(177, 178, 186, 170, 175, 188, 181) ## Populate with the heights of men from class
 women_heights <- c(160, 167, 169, 163, 170, 168, 171) ## Populate with the heights of women from class
+men_heights <- c(177, 178, 186, 170, 175, 188, 181) ## Populate with the heights of men from class
+women_heights <- c(160, 167, 169, 163, 170, 168, 171) ## Populate with the heights of women from class
 
 # Write a Welch's t-test function for any two samples x1 and x2
 my_t <- function(x1,x2){
@@ -289,19 +303,28 @@ my_t <- function(x1,x2){
   n2 <- length(women_heights)
   m2 <- mean(women_heights)
   s2 <- sd(women_heights)
+  n1 <- length(men_heights)
+  m1 <-mean(men_heights)
+  s1 <- sd(men_heights)
+  n2 <- length(women_heights)
+  m2 <- mean(women_heights)
+  s2 <- sd(women_heights)
  
   # next, calculate the average standard deviation using the formula shown in the class on slide 44:
  
   s <- sqrt(((s1^2)/n1) + ((s2^2)/n2))
+  s <- sqrt(((s1^2)/n1) + ((s2^2)/n2))
 
   # next, calculate the t-statistic, again as shown on slide 44
  
+  t <- (m1 - m2) / s
   t <- (m1 - m2) / s
  
  
   # next, calculate the degrees of freedom (again see slide 44)
   # make sure you use parentheses correctly here
  
+  df <- ((((s1^2)/n1) + ((s2^2)/n2))^2) / (((((s1^2)/n1)^2)/(n1-1)) + (((s2^2)/n2)^2)/(n2-1))
   df <- ((((s1^2)/n1) + ((s2^2)/n2))^2) / (((((s1^2)/n1)^2)/(n1-1)) + (((s2^2)/n2)^2)/(n2-1))
  
   # next, calculate the probability that the t-statistic would be greater than the absolute value of the t-statistic that you calculated if the TRUE difference between the groups was 0
