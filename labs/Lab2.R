098c97d4d8d1b4382ca8eb4cb9b243c0f07fa60d
Yeboah10 <acquahyeboah01@gmail.com>
2024-10-28 23:49:21 +0100

    Exercise completed

--- labs/Lab2.R
+++ labs/Lab2.R
@@ -56,7 +56,10 @@ total_revenue
 ### 1.2
 # Rename the 3rd element of the column names of star_wars_matrix to "Total revenue"
 colnames(star_wars_matrix)[3]<- "Total revenue"
+<<<<<<< HEAD
 star_wars_matrix
+=======
+>>>>>>> 4d768c0 (Exercise completed)
 # Now lets create vectors for the box office returns of the prequel trilogy
 
 phantom_menace <- c(474.5,552.5)
