cc2cc67293bf8226b046eb9338d99ee646c4cc30
Edmond Seabright <ed.seabright@gmail.com>
2024-10-29 11:22:17 +0100

    added lab 3 and my answers

--- labs/submissions/Lab 3/Lab3_Seabright.R
+++ labs/submissions/Lab 3/Lab3_Seabright.R
@@ -8,6 +8,9 @@
 ###################################
 ###################################
 
+## Class heights
+
+men <- c(174,180,178,185,178,189,175,165,174,197,171,163,173)
 
 # Some exercises to practice what we learned this week.
 
@@ -16,12 +19,7 @@
 #    Amir is 194cm tall. In what height percentile of the population is he? Round to the nearest integer number.
 #    (Remember: 1st percentile = shortest 1 percent of the population, 99th percentile: tallest 1 percent of the population)
 
-
-
-
-
-
-
+# Amir is in the 95th percentile
 
 
 
@@ -29,13 +27,25 @@
 #    OR the bottom 2.5% of the population in height to be let in. 
 #    How tall or how short do you have to be to be accepted into the TSC? Would Amir be accepted?
 
+# You would have to be either 140.6 cm or shorter OR
+# 199.3cm or taller.
+
+# Amir would not be accepted
+
 # 3. In response to the Tall and Short club, another group of Examplians start the Average People's Club (APC), where you have to be
 #    within 0.1 standard deviations from the mean in height in order to get in.
 #    What is the range of heights acceptable to join the APC? What percentage of the total population is eligible to join?
 
+mean = 170
+sd = 15
+upper_limit = 171.5
+lower_limit = 168.5
 
 # 4. If we selected 10 Examplians at random from the population, what is the probability that none of them are eligible to join either
 #    the TSC or the APC? 
+p_not_eligible = 0.87
+p_10_not_eligible = p_not_eligible^10
+
 
 # 5. What is the probability that exactly 2 are eligible to join the APC and the rest are not?
 
