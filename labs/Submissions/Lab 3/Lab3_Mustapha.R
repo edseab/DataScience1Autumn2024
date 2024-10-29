69b286df1c6cbb1221e0d47cb47a4fbb17e48186
Li2angel <mbaalade99@gmail.com>
2024-10-29 11:54:08 +0100

    saved Lab 3 in my branch

---
+++ labs/Submissions/Lab 3/Lab3_Mustapha.R
@@ -0,0 +1,40 @@
+###################################
+###################################
+########                   ########
+########   Data Science 2  ########
+########       Lab 3       ######## 
+########  29th Oct. 2024   ########
+########                   ########
+###################################
+###################################
+
+
+# Some exercises to practice what we learned this week.
+
+# 1. In the country of Examplia, it is known that people's heights are precisely normally distributed,
+#    with a mean of 170cm and a standard deviation of 15cm.
+#    Amir is 194cm tall. In what height percentile of the population is he? Round to the nearest integer number.
+#    (Remember: 1st percentile = shortest 1 percent of the population, 99th percentile: tallest 1 percent of the population)
+
+
+# 2. A group of Examplians start a club called the Tall and Short Club (TSC), where you have to be in the tallest 2.5% of the population 
+#    OR the bottom 2.5% of the population in height to be let in. 
+#    How tall or how short do you have to be to be accepted into the TSC? Would Amir be accepted?
+
+# 3. In response to the Tall and Short club, another group of Examplians start the Average People's Club (APC), where you have to be
+#    within 0.1 standard deviations from the mean in height in order to get in.
+#    What is the range of heights acceptable to join the APC? What percentage of the total population is eligible to join?
+
+mean <-  170
+sd <- 15
+upper_limit <- 171.5
+lower_limit <- 168.5
+(pnorm(0.1) - pnorm(-0.1))*100
+
+# 4. If we selected 10 Examplians at random from the population, what is the probability that none of them are eligible to join either
+#    the TSC or the APC? 
+p_not_eligible <- 1 - 0.05 - 0.08
+p_10 <- p_not_eligible ^ 10
+# 5. What is the probability that exactly 2 are eligible to join the APC and the rest are not?
+
+# 6. What is the probability that at least 3 of them are eligible to join the TSC?
