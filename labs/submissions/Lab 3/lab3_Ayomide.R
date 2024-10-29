3d2d0aa5f7ee50a0cb641c50c4b3ad06c4cf71c7
ayomide-fagbolade <ayomide.fagbolade@um6p.ma>
2024-10-29 21:19:56 +0100

    question 1-4 done

--- labs/submissions/Lab 3/lab3_Ayomide.R
+++ labs/submissions/Lab 3/lab3_Ayomide.R
@@ -54,6 +54,12 @@ qnorm(((100-2.5)/100),170,15)
 #    What is the range of heights acceptable to join the APC? What percentage of the total population is eligible to join?
 
 
+# One has to be from 168.5 to 171.5 cm in height to belong to the Average People's Club 
+pnorm(171.5,170,15)- pnorm(168.5,170,15)
+
+# Just about 8 percent of the population is eligible to join the average people club
+
+
 # 4. If we selected 10 Examplians at random from the population, what is the probability that none of them are eligible to join either
 #    the TSC or the APC? 
 1-(2.5+2.5+8)/100
