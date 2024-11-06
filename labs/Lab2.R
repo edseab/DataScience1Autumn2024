c091278cfc987b2a0273b4ab30338bc60c6c31e2
emmanuel <oseimanuel30@gmail.com>
2024-11-06 23:39:37 +0100

    new commit

--- labs/Lab2.R
+++ labs/Lab2.R
@@ -48,7 +48,7 @@ colnames(star_wars_matrix) <- c("US revenue", "International revenue")
 # Some important base R functions to know: colSums, rowSums, colMeans, rowMeans
 # Use one of the above functions to calculate the total revenue for each movie (the sum of the US and international revenue)
 # and save it in an object called total_revenue
-
+total_revenue
 # We can now add this vector as a new column using the function cbind (column bind)
 
 star_wars_matrix <- cbind(star_wars_matrix, total_revenue)
