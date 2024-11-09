63c24f02bf1df48804800aa23efb5d20162bd7fb
Oluwafemi1hub <oluwafemioniosun1@gmail.com>
2024-11-10 00:40:22 +0100

    Practice.R

--- labs/Oluwafemi/Practice/Practice.R
+++ labs/Oluwafemi/Practice/Practice.R
@@ -1,3 +1,23 @@
 ?C
 A <- c(1,2,3,4,5,6,7,8,9)
 c(1,2,3,4,5,6,7,8,9)
+drink_vector <- (c('milk','water','juice'))
+drink_vector
+drink_factor <- factor(drink_vector)
+drink_vector
+d <- c('Hello world', 'R is fun!')
+d
+print (d) 
+drink_vector <- (c('milk', 'water', 'juice'))
+drink_vector
+# [1] "milk"  "water" "juice"
+drink_factor <- factor(drink_vector)
+drink_factor
+# [1] milk  water juice
+# Levels: juice milk water
+a <- c(1, 3, 5, 7)
+b <- c(2, 4, 6, 8)
+df<-data.frame(a, b)
+df
+setwd("d: /reasercher")
+unlink()
