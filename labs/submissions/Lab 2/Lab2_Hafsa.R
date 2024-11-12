202b446d4b94639245e0343a7773a6ce9b9b3fd6
hafsaSaih <126255352+hafsaSaih@users.noreply.github.com>
2024-11-12 12:10:55 +0100

    Hafsa-SAIH (#60)
    * modifications
    
    * fix files
    
    * added lab 3
    
    * lab 3
    
    * fix files
    
    * modifications
    
    * fix files
    
    * added lab 3
    
    * lab 3
    
    * fix files
    
    * lab 4 done

--- labs/submissions/Lab 2/Lab2_Hafsa.R
+++ labs/submissions/Lab 2/Lab2_Hafsa.R
@@ -17,9 +17,12 @@
 
 # In R, a MATRIX is a vector that has folded onto itself into a series of rows and columns. You can construct one from a normal vector:
 matrix(1:9, nrow = 3)
+
 ?matrix
 
 # By default matrices get filled column by column, as you can see from the output. To fill them row by row instead, change the argument brow to TRUE:
+
+
 
 matrix(1:9, byrow = T, nrow = 3)
 
@@ -51,8 +54,10 @@ colnames(star_wars_matrix) <- c("US revenue", "International revenue")
 # Use one of the above functions to calculate the total revenue for each movie (the sum of the US and international revenue)
 # and save it in an object called total_revenue
 
+
 total_revenue <- rowSums(star_wars_matrix)
 total_revenue
+
 
 
 # We can now add this vector as a new column using the function cbind (column bind)
@@ -62,7 +67,9 @@ star_wars_matrix <- cbind(star_wars_matrix, total_revenue)
 ### 1.2
 # Rename the 3rd element of the column names of star_wars_matrix to "Total revenue"
 
+
 colnames(star_wars_matrix) <- c("US revenue", "International revenue", "Total revenue")
+
 
 
 
@@ -81,6 +88,7 @@ my.matrix <- cbind(my.matrix,rowSums(my.matrix))
 rownames(my.matrix) <- c("The Phantom Menace", "The attack of clones", "revenge of sith")
 star_wars_matrix <- rbind(star_wars_matrix,my.matrix)
 
+
 # Matrices are understood by R to be both one-dimensional, because they are vectors folded onto themselves
 # into columns, but also 2 dimensional, because they have rows and columns. 
 # So you can index them like this:
@@ -94,7 +102,9 @@ star_wars_matrix[3,1]
 
 ### 1.4 
 # Write a line of code to extract the international revenue of the Phantom Menace (the 4th movie) using numbers to index the matrix
+
 star_wars_matrix[4,2]
+
 
 # Compare this to the following:
 star_wars_matrix["The Phantom Menace", "Total revenue"]
@@ -142,7 +152,9 @@ my_list$boolean
 
 ### 2.1
 # Using the $ operator, replace the "matrix" element of my_list with the star_wars_matrix
+
 my_list$matrix <-star_wars_matrix
+
 
 # Finally, you can turn any list into a vector with unlist().
 unlist(my_list)
@@ -228,6 +240,7 @@ sample(1:100,1)
 # To do this you will need to use either the factorial() function
 # or the choose() function
 
+
 success.probability <- function(n,k,p){
   return(p^k*(1-p)^(n-k)*choose(n,k))
 }
@@ -246,6 +259,7 @@ success.probability(10,8,0.7)
 
 # compare this to dbinom(8,10,0.7)
 dbinom(8,10,0.7)
+
 
 ### PROBABILITY FUNCTIONS IN R 
 # dbinom, dnorm, dunif, dbeta, .... all of these functions calculate f(x) for any given x
@@ -268,6 +282,7 @@ dbinom(8,10,0.7)
 # Using rbinom(), simulate 100,000 universes where the aliens sent out 20 probes to Earth
 # and calculate in what percentage of these universes the number of probes signalling Water is 11 or fewer
 # What do you conclude to the astronomer?
+
 
 rbinom(20,100000,0.7)
 proba.11.or.fewer <- sum(rbinom(20,100000,0.7) <=11)/100000
@@ -296,12 +311,16 @@ qnorm(0.1,175, 10, lower.tail=FALSE)
 ### 4.3
 # Let's run a Welch's t-test comparing the heights of men and women in class
 
+
 men_heights <- c(175, 180, 178, 172, 185, 177, 183, 174, 179, 181) #random heights
 women_heights <- c(165, 162, 170, 168, 160, 172, 167, 164, 169, 161) #random heights
+
+
 
 # Write a Welch's t-test function for any two samples x1 and x2
 my_t <- function(x1,x2){
   # first, extract the means, variances and Ns of the two samples and save thel to
+
   n1 <- length(x1)
   m1 <- mean(x1)
   s1 <- var(x1)
@@ -316,12 +335,17 @@ my_t <- function(x1,x2){
   # next, calculate the t-statistic, again as shown on slide 44
  
   t <- (m1 - m2) / s
+
+ 
+
  
  
   # next, calculate the degrees of freedom (again see slide 44)
   # make sure you use parentheses correctly here
  
+
   df <- ((s1 / n1) + (s2 / n2))^2 / (((s1 / n1)^2 / (n1 - 1)) + ((s2 / n2)^2 / (n2 - 1)))
+
  
   # next, calculate the probability that the t-statistic would be greater than the absolute value of the t-statistic that you calculated if the TRUE difference between the groups was 0
   # to do this, you can use function pt
