d5da581e71f3c8423b27f9aeef1d7b9d5a1bf096
ayomide-fagbolade <ayomide.fagbolade@um6p.ma>
2025-01-07 12:48:10 +0100

    starting lab 8

---
+++ labs/Submissions/Lab 7/Ayomide_lab 8.r
@@ -0,0 +1,150 @@
+
+###################################
+###################################
+########                   ########
+########   Data Science 1  ########
+########       Lab 8       ######## 
+########  17th Dec. 2024   ########
+########                   ########
+###################################
+###################################
+
+## Welcome to lab #8. 
+
+# Lets start by learning about p-hacking
+
+
+# 1. THE ICE BATH STUDY
+# In 2022 a paper was published in a medical journal that claimed to show 
+# that taking ice-cold baths could have health benefits. They asssigned 50%
+# of their subjects to the treatment group and received the ice baths
+# and 50% to a control group that received no ice cold baths 
+# (they could have received warm baths instead, but this study didn't do this)
+# The paper abstract boasted that the ice baths had statistically significant (at the 5% level)
+# effects on 3 health outcomes (perceived energy levels, mental alertness, probability of catching a cold within 1 week of treatment)
+# Looking into the methods section of the paper, we can see that they actually 
+# tested 60 different possible health outcomes (physical strength, skin health, blood pressure...)
+
+# Could they have gotten these results purely by chance?
+''' i Think so since only the results of 3 tests were reported'
+# Let's simulate the cold-water therapy experiment
+# 1.1 Start by saving a sample size of 100 to an object N
+
+N <- 100
+
+
+# 1.2 Next, create a vector of 1s and 0s, so that half of your sample receives the treatement (coded 1), and the other half doesn't (coded 0)
+
+vectorr<- c(rep(1,N/2),rep(0,N/2))
+# Next we will run 60 different t-tests using a for loop, and save the p-value for each of these tests
+# 1.3 start by initializing an empty vector for your p-values using an empty c() function. 
+p_val<- c()
+
+y<- c(rnorm(N))
+y[vectorr==1]
+t.test()
+
+# 1.4 Next, write a for loop for a total of 60 loops
+# For each loop, save a new y-variable that you have sampled completely at random
+# your y-variable should be of length N, but otherwise you can sample it however you like (rnorm,runif,rbinom... any probability distribution you like with any parameter values you like)
+for(i in 1:60){
+    y<- c(rnorm(N))
+
+
+# Next (still within the for loop), run a t-test comparing the y-values corresponding to treatment == 1 with the y-values corresponding to treatment ==0
+    t_test<- t.test(y[vectorr==1],y[vectorr==0])
+# extract the p-value for that t-test and save it to the vector of p-values
+    p_val[i]<-t_test$p.value
+}
+
+# 1.5 After your for-loop is run, calculate the sum of the saved p-values that are less than 0.05
+sum(p_val[p_val<0.05])
+# 1.6 How many significant results are there at the 0.05 level?
+"0.0865547"
+length(p_val[p_val<0.05])
+
+# 1.7 Run the whole for loop again several times. How many significant results do you find each time?
+
+# 1.8 Write a for loop that runs the previous for-loop multiple times, and save the number of significant results you get each time. Run this for loop at least 100 times
+
+
+# 1.9 Plot the histogram of the number of significant results you get for each multiple comparisons experiment. What is the average number?
+
+
+# 1.10 Run this whole code again, but this time increase the sample size N. Does your histogram look any different? why?
+
+# 1.11 Run the code again, but this time instead of counting how many results are less than 0.05, divide this threshold by the total number of comparisons (60).
+
+
+
+# 1.12 What does your new histogram look like?
+
+# 1.13 This (dividing the p-value required for significance by the number of comparisons made) is called Bonferroni correction for multiple comparisons.
+# Why is it important?
+
+
+# 2. Partitioned regression
+
+# 2.1 Run a multiple regression using the mtcars database with fuel efficiency (mpg)
+# as the dependent (outcome) variable and weight (wt) and number of cylinders (cyl)
+# as independent (predictor) variables
+# Look at the output of this regression and note the value of the parameter estimates for the effects of wt and cyl.
+
+# 2.2 save the residuals of the regression with formula (mpg ~ wt) into an object called mpg_wt_residuals
+
+# 2.3 Interpret this object. What does it represent?
+
+# 2.4 Save the residuals of the regression with formula (cyl ~ wt) into an object called cyl_wt_residuals
+
+# 2.5 Interpret this object. What does it represent?
+
+# 2.6 Run a regression with the formula (mpg_wt_residuals ~ cyl_wt_residuals)
+
+# 2.7 Based on your answers to 2.3 and 2.5, how do you interpret the slope estimate of the previous regression?
+
+# 2.8 Compare this estimate to the estimate of wt in the original full model. 
+# What do you conclude?
+
+# 2.9 Use the same method to get the estimate of cyl from the original model using only simple linear regressions.
+
+
+
+#############################
+##                         ##
+##        HOMEWORK         ##
+##                         ##
+#############################
+
+library(dplyr)
+
+# 3.1 Load the dataset 'toycars' from the package 'DAAG'
+install.packages('DAAG')
+library(DAAG)
+data(toycars)
+# This dataset contains information about experiments launching toy cars at different angles and measuring the distances they travel before falling to the ground.
+# Learn more about this dataset by looking up ?toycars
+
+# There are 3 different types of toy cars, numbered 1, 2, and 3
+
+# We want to model the relationship between the type of car and the distance they travel. 
+# 3.2 Which variable should be the outcome (independent) variable? Why?
+
+# 3.3 Run a regression model with the formula distance ~ car. How would you interpret the regression coefficient? What is the problem with this model? How would you rectify this problem?
+
+# 3.4 Recode the 'car' variable so that car 1 is coded as 'green', car 2 is 'yellow', and car 3 is 'red'
+
+# 3.5 Rerun the regression model using this recoded variable and interpret the coefficients. What does this model say about whether how choosing different cars affects the distances they travel?
+
+# 3.6 Rerun the model again, this time including 'angle' as an independent variable. Interpret all 4 coefficients.
+
+# 3.7 What is the predicted distance traveled by a red car launched at 3 degrees?
+
+# 3.8 What is the predicted distance traveled by a white car launched at 2.5 degrees?
+
+# 3.9 Plot the results of this model using ggplot with 90% confidence intervals and appropriate colors
+
+
+
+
+
+
