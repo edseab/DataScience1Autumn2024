b0b66f0273ac35a8d8bc8ee6dafa473d27582d1c
Uzoma Owuama <owuamuzoma@gmail.com>
2025-01-28 01:50:02 +0100

    Your commit message

--- labs/Submissions/Lab 7/Lab 7_Uzoma.r
+++ labs/Submissions/Lab 7/Lab 7_Uzoma.r
@@ -21,10 +21,10 @@
 # It signifies: take the element on the left and use it as the first argument in the function on the right
 
 # Example
-set.seed(123)
+
 hist(rnorm(200), breaks=seq(-4,4,0.5))
 # is equivalent to
-set.seed(123)
+
 rnorm(200) |> hist(breaks=seq(-4,4,0.5))
 
 # The purpose of the pipe is mostly aesthetic, in particular to avoid large numbers of parentheses when something needs to be transformed using multiple functions:
@@ -35,8 +35,7 @@ rnorm(200) |> hist(breaks=seq(-4,4,0.5))
 set.seed(123)
 round(sqrt(log(runif(10,1,10))),2)
 
-pipe_exp <- runif(10,1,10) |> log() |> sqrt() |> round(2)
-
+runif(10,1,10) |> log() |> sqrt() |> round(2)
 
 # Pipes were initially created in a package called magrittr, part of the 'tidyverse' group of packages
 
@@ -49,7 +48,7 @@ pipe_exp <- runif(10,1,10) |> log() |> sqrt() |> round(2)
 
 # Here we will install tidyverse, which is not one but a series of packages written by the same group of people, which share a number of syntactical features
 
-install.packages("tidyverse")
+install.packages('tidyverse')
 
 # After you have installed tidyverse into your packages, you must also load it into your environment
 
@@ -100,9 +99,12 @@ mtcars %>%
   select(1:5)
 
 ### 2.1
-# using select() and filter(), create a new database of cars that are over 4000 lbs in weight, retaining only the wt and mpg columns. Save this database to an object called 'df'.
-df <- mtcars %>% filter(wt>4) %>% select(1,6)
+# using select() and filter(), create a new database of cars that are over 4000 lbs in weight, retaining only the wt and mpg columns.
+# Save this database to an object called 'df'.
 
+mtcars %>%
+  filter(wt>=4) %>%
+  select(c('wt',mpg)) -> df
 
 # After you have selected the rows and columns you are interested in, you can 
 # change the order of the rows using arrange
@@ -114,13 +116,13 @@ df %>% arrange(desc(wt))
 
 # To change variables, we can use mutate()
 df <- df %>% mutate(wt_kg=wt*453.592,
-                    km_per_l = mpg*1.60934/3.78541) ##adds a column with mentioned parameters
+                    km_per_l = mpg*1.60934/3.78541)
 
 # And we can use ifelse() within mutate()
 mtcars <- mtcars %>%
             mutate(wt_class = ifelse(wt>=4, 'Oversized','Standard'))
 
-# We can even do a sultiple ifelse statement using case_when()
+# We can even do a sultiple ifelse statment using case_when()
 mtcars <- mtcars %>%
             mutate(
               efficiency = case_when(
@@ -176,22 +178,23 @@ data(iris)
 # For each of the different species of iris, present the mean and standard deviation for the sepal length, sepal width, and petal area, as well as the number of samples (n)
 # Order this database in decreasing order of average petal length.
 
-iris <- iris %>%
-            mutate (
-              Petal.Area = Petal.Width*Petal.Length
-            ) %>%
-            group_by(Species) %>%
-            summarise(
-              mean_sepal_len = mean(Sepal.Length),
-              sd_sepal_len = sd(Sepal.Length),
-              mean_sepal_width = mean(Sepal.Width),
-              sd_sepal_width = sd(Sepal.Width),
-              mean_petal_area = mean(Petal.Area),
-              sd_petal_area = sd(Petal.Area),
-              n = n(),
-              mean_petal_len = mean(Petal.Length)
-            ) %>%
-            arrange(desc(mean_petal_len))
+iris %>% 
+  mutate(Petal.Area = Petal.Length * Petal.Width) %>% 
+  group_by(Species) %>% 
+  summarise(
+    mean_Sepal.Length = mean(Sepal.Length),
+    sd_Sepal.Length = sd(Sepal.Length),
+    mean_Sepal.Width = mean(Sepal.Width),
+    sd_Sepal.Width = sd(Sepal.Width),
+    mean_Petal.Length = mean(Petal.Length),
+    sd_Petal.Length = sd(Petal.Length),
+    mean_Petal.Width = mean(Petal.Width),
+    sd_Petal.Width = sd(Petal.Width),
+    mean_Petal.Area = mean(Petal.Area),
+    sd_Petal.Area = sd(Petal.Area),
+    n=length(Sepal.Length)
+  ) %>% 
+  arrange(desc(mean_Petal.Length)) -> iris
 
 
 #######################################################
@@ -211,10 +214,10 @@ my.lm <- function(y,x){
 # Start by saving the sample size (n). You will need it later for calculating degrees of freedom for the test statistics and sampling distributions.
   n <- length(y)
 # Next create the X matrix using as.matrix. Don't forget to add the column of 1s!
-  x <- as.matrix(cbind(1,x))
+  x <- as.matrix(cbind(1, x))
 # use the y input and your newly constructed x matrix to calculate the parameter estimates (or coefficients). 
 # Hint: see lab 6, and remember that t() gives the transpose of a matrix and solve() gives its inverse
-  b <- solve(t(x) %*% x) %*% t(x) %*% y
+  b <- solve(t(x)%*%x)%*%t(x)%*%y
 # Next, using these calculated coefficients, calculate all of the predicted values, y_hat, for each of the values of x (each of the rows of the X matrix). 
 # Hint: look at the formula for the linear model - and remember we are multiplying matrices!
   y_hat <- x %*% b
@@ -223,23 +226,23 @@ my.lm <- function(y,x){
 # Next calculate the estimate of the residual standard error, s. Careful about the denominator here: check how many degrees of freedom you have!
   s <- sqrt(sum(e^2)/(n-ncol(x)))
 # Using s, calculate the variance covariance matrix of the coefficients.
-  vcov_coef <- (s^2)*solve(t(x)%*%x)
+  vcov_coef <- s^2 * solve(t(x) %*% x)
 # from this covariance matrix, extract the standard errors of the coefficients
   se_coef <- sqrt(diag(vcov_coef))
 # use these standard errors to calculate the t-values for each of the coefficients
   t_vals <- b/se_coef
 # And use these t-values to calculate p-values.
-  p <- (1-pt(abs(t_vals), df=n-ncol(x)))
+  p <- 1 - pt(abs(t_vals),df=n-ncol(x))
 # Next, calculate the R2 value
 
   RSS <- sum(e^2)
-  TSS <- sum((y-mean(y))^2)
-  r2 <- 1-(RSS/TSS)
+  TSS <- sum((y - mean(y))^2)
+  r2 <- 1 - RSS / TSS
 # Bonus points if you want to look up how to calculate adjusted R2
-  adj_r2 <- 1-((1-r2)*(n-1)/(n-ncol(x)))
+  adj_r2 <- 1 - (RSS / (n - ncol(x) - 1)) / (TSS / (n - 1))
 # Next, calculate the F-statistic and the p-value for the model.
-  f_stat <- ((TSS-RSS)/(ncol(x)-1))/(RSS/(n-ncol(x)))
-  model_p <- 1-pf(f_stat, df1=ncol(x)-1, df2=n-ncol(x))
+  f_stat <- (TSS - RSS) / (ncol(x) - 1) / (RSS / (n - ncol(x)))
+  model_p <- pf(f_stat, ncol(x) - 1, n - ncol(x), lower.tail = F)
   
 # Finally, put all of these elements into a list, and have your function return that list. 
   return(list(n=n,
@@ -253,14 +256,12 @@ my.lm <- function(y,x){
               r2 = r2,
               adj_r2 = adj_r2,
               F_statistic = f_stat,
-              model_p_value = model_p
-              df=n-p))
+              model_p_value = model_p))
 }
 
 # compare your function against the built-in R function in R.
 
 my.lm(y=mtcars$mpg,x=mtcars[,c('wt','cyl')])
-summary(lm(mpg ~ wt+cyl, data=mtcars))
 
 ### 4.2 predict()
 
@@ -272,26 +273,25 @@ my.predict <- function(model_output, new_data, ci_level = 0.97) {
   coef = model_output$coefficients
 
   # Add a column of 1s to the new_data
-  new_data = cbind(1, as.matrix(new_data))
+  new_data = cbind(1,new_data)
 
   # generate the predicted values of y from the new_data object based on the coefficients from the model
   # Again, remember the formula for the linear model!
-  y_hat = new_data %*% coef
+  y_hat = new_data%*%coef
 
 #for each of these predicted values, calculate a confidence interval
   # first calculate the critical levels of the t-distribution using the ci_level and the degrees of freedom
-  df=model_output$df
   area_in_tails = (1-ci_level)/2
-  t_lower = qt(area_in_tails, df=df)
-  t_upper = qt((1-area_in_tails),df=df) #you can also say that t_upper = -t_lower
+  t_lower = qt(area_in_tails,df=model_output$n-ncol(new_data))
+  t_upper = -t_lower
 
   # next, for each row of the new_data object, calculate the standard error of the predicted value
   # Check the slides, and remember that 𝒗𝒄𝒐𝒗(𝒃) was saved as "vcov" in the model output
  sy = sqrt(apply(new_data,1,function(.)t(.)%*%model_output$vcov%*%.))
   
   # next, calculate the upper and lower confidence boundaries for each prediction using the critical t-levels and the previously calculated standard error
- ci_lower = y_hat + (t_lower*sy)
- ci_upper = (t_upper*sy)
+ ci_lower = y_hat + t_lower.*sy
+ ci_upper = y_hat + t_upper.*sy
 
   # finally, put these confidence intervals into a dataframe
  ci = data.frame(ci_lower,ci_upper)
@@ -305,16 +305,21 @@ my.predict <- function(model_output, new_data, ci_level = 0.97) {
 # Use your functions to run the following model
 
 # mtcars$mpg ~ mtcars$wt + mtcars$cyl
-model<-my.lm(y=mtcars$mpg,x=mtcars[,c('wt','cyl')])
+model<- my.lm(y=mtcars$mpg,x=mtcars[,c('wt','cyl')])
 
 # Then plot predicted marginal effect of weight for a car with 4 cylynders. Add 96% confidence intervals to the graph.
 # Hint: For this, in your "newdata" object, add a column of 4s for the number of cylinders
-x<-seq(0,6,0.1)
-ci<-my.predict(model,new_data=cbind(wt))
-# Compare this graph to the graph from the lecture.
+x <- seq(0,6,0.1)
 
+ci <- my.predict(model,new_data=cbind(wt=x,cyl=4),ci_level=0.96)
+
+plot(mtcars$wt,mtcars$mpg, pch=20)
+lines(x,model$coefficients['1',]+x*model$coefficients['wt',]+4*model$coefficients['cyl',])
+
+polygon(x=c(x,rev(x)),y=c(ci$'0.96%_lower',rev(ci$'0.96%_upper')),col=adjustcolor('slateblue',alpha.f=0.4))
+
+# Compare this graph to the graph from the lecture.
+#The slope is less steep. 
 
 # What do you conclude?
-
-
-
+# This indicates that number of cylinders partially confounds the relationship between car weight and car efficiency. Once cylinders are controlled for, the effect of weight on efficiency is less pronounced, although still important and significant.
